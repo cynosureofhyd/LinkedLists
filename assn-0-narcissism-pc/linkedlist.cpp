f1054b8228a4963642f254d619124c9311f2337f
cynosureofhyd2 <tomu646@gmail.com>
2013-11-03 13:48:51 -0700

    Add Node correct implementation

--- assn-0-narcissism-pc/linkedlist.cpp
+++ assn-0-narcissism-pc/linkedlist.cpp
@@ -15,10 +15,12 @@ struct node
 };
 
 struct node* CreateNode(int value);
+node* AddNode(node* currentNode, int value);
 
 int main()
 {
 	node* newNode = CreateNode(20);
+	node* anotherNode = AddNode(newNode, 30);
 	return 0;
 }
 
@@ -36,7 +38,7 @@ node* AddNode(node* currentNode, int value)
 	if(currentNode == NULL)
 		return CreateNode(value);
 	node* newNode = CreateNode(value);
-	newNode->next = currentNode->next;
+	newNode->next = currentNode;
 	currentNode = newNode;
 
 	return currentNode;
